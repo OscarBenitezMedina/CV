f7aff20e20eec56279b9f87e9c10da3036a53343
unknown <oscarbenitezmedina99@gmail.com>
2021-07-29 11:25:13 +0200

    first comit

---
+++ js/buttons.js
@@ -0,0 +1,12 @@
+function Button1() {
+    location.href="AboutMe.html";
+}
+function Button2() {
+    location.href="Skills.html";
+}
+function Button3() {
+    location.href="Projects.html";
+}
+function Button4() {
+    location.href="ContactMe.html";
+}
